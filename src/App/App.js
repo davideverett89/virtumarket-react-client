15451ffb706c6a3c51621aae8b69e5bdc7896c22
David Everett <davideverett1989@gmail.com>
2020-09-10 10:16:58 -0500

    Inital setup complete.

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => (
+  <div className="App">
+    <h2>INSIDE APP COMPONENT</h2>
+    <button className="btn btn-info">I am a button</button>
+  </div>
+);
+
+export default App;
