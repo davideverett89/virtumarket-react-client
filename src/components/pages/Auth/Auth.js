043841a87e71d0813d660e6b72a972248d6d755b
David Everett <davideverett1989@gmail.com>
2020-09-11 14:21:59 -0500

    Navbar is displaying buttons only when logged in, and routing to the appropriate components.

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -9,8 +9,18 @@ const Auth = ({ setAuthed }) => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <div className="Auth">
-        <button type="button" className="btn btn-primary" onClick={() => setToggle(!toggle)}>Change Form</button>
+    <div className="Auth mt-5">
+      <h1 className="display-4">Welcome to VirtuMarket!</h1>
+        <div class="bs-example m-5">
+          <div class="btn-group btn-group-toggle" data-toggle="buttons">
+              <label class={`btn btn-success ${toggle ? 'active' : ''}`}>
+                  <input type="radio" name="options" autocomplete="off" onClick={() => setToggle(true)} /> Login
+              </label>
+              <label class={`btn btn-success ${!toggle ? 'active' : ''}`}>
+                  <input type="radio" name="options" autocomplete="off" onClick={() => setToggle(false)} /> Sign Up
+              </label>
+          </div>
+        </div>
         {
             toggle
               ? (<UserAuthenticationForm setAuthed={setAuthed} />)
--- src/components/shared/MyNavBar/MyNavBar.js
+++ src/components/shared/MyNavBar/MyNavBar.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { NavLink as RRNavLink } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
 import useSimepleAuth from '../../../helpers/data/authData';
 
 import './MyNavBar.scss';
 
-const MyNavBar = ({ setAuthed }) => {
+const MyNavBar = ({ authed, setAuthed }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
   const { logout } = useSimepleAuth();
   const logMeOut = (e) => {
     e.preventDefault();
@@ -12,10 +26,62 @@ const MyNavBar = ({ setAuthed }) => {
     setAuthed(false);
   };
   return (
-        <div className="MyNavbar">
-            <h1>Navbar</h1>
-            <button className="btn btn-dark logout-btn" onClick={logMeOut}>Logout</button>
-        </div>
+    <div className="MyNavbar">
+      <Navbar color="success" light expand="md">
+        <NavbarBrand href="/">
+          <h3 className="text-white">VirtuMarket App</h3>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+          {
+            authed && localStorage.getItem('userRole') === 'merchant'
+              ? (
+              <Nav className="ml-auto" navbar>
+                <NavItem className="mx-1">
+                  <NavLink className="btn nav-btn" tag={RRNavLink} to='/home'>Merchant Home</NavLink>
+                </NavItem>
+                <NavItem className="mx-1">
+                  <NavLink className="btn nav-btn" tag={RRNavLink} to='/goods/add'>Add Good</NavLink>
+                </NavItem>
+                <NavItem className="mx-1">
+                  <NavLink className="btn nav-btn" tag={RRNavLink} to='/accounts'>Profile</NavLink>
+                </NavItem>
+                <NavItem className="mx-1">
+                  <NavLink className="btn nav-btn logout-btn" onClick={logMeOut}>Logout</NavLink>
+                </NavItem>
+              </Nav>
+              )
+              : (
+                ''
+              )
+          }
+          {
+            authed && localStorage.getItem('userRole') === 'consumer'
+              ? (
+              <Nav className="ml-auto" navbar>
+                <NavItem className="mx-1">
+                  <NavLink className="btn nav-btn" tag={RRNavLink} to='/home'>Consumer Home</NavLink>
+                </NavItem>
+                <NavItem className="mx-1">
+                  <NavLink className="btn nav-btn" tag={RRNavLink} to='/basket'>Basket</NavLink>
+                </NavItem>
+                <NavItem className="mx-1">
+                  <NavLink className="btn nav-btn" tag={RRNavLink} to='/accounts'>Profile</NavLink>
+                </NavItem>
+                <NavItem className="mx-1">
+                  <NavLink className="btn nav-btn logout-btn" onClick={logMeOut}>Logout</NavLink>
+                </NavItem>
+              </Nav>
+              )
+              : (
+                ''
+              )
+          }
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 };
 
